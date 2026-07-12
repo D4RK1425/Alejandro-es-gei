36444380308c007ff64556661c35b4a63bd0b15b
D4RK1425 <dark041425@gmail.com>
2026-07-12 12:22:01 -0500

    piso corregido

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED := 120.0
 const RUN_SPEED := 200.0
-const JUMP_VELOCITY := -300.0
+const JUMP_VELOCITY := -350.0
 const GRAVITY := 800.0
 const INVULNERABILITY_TIME := 0.5
 
